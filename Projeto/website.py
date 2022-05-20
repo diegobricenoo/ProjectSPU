60fb5ba035e4713dd5036dc680ca10b59e6a51d7
Ruben Rodrigues <ruben2000@gmail.com>
2022-05-20 10:19:14 +0100

    Arduino in website

--- Projeto/website.py
+++ Projeto/website.py
@@ -9,21 +9,20 @@
 #     return 'Hello, World!'
 
 import serial
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
 ser.reset_input_buffer()
 
 if __name__=='__main__':
-    app.run(host='0.0.0.0')
-
-
-@app.route('/')
-def hello():
     while True:
         # ser.write(b"Hello from Raspberry Pi!\n")
         if ser.in_waiting>0:
             line = ser.readline().decode('utf-8').rstrip()
             print(line)
-    return 'Hello, World!'
+
+
+@app.route('/')
+def hello():
+    return render_template('index.html')
