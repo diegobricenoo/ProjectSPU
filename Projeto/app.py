7f8ea50d97dc7daf58ac3f40e29e47dbe09177dd
Ruben Rodrigues <ruben2000@gmail.com>
2022-05-24 23:00:00 +0100

    Button to submit form

---
+++ Projeto/app.py
@@ -0,0 +1,94 @@
+# from flask import Flask
+
+
+# app = Flask(__name__)
+
+
+# @app.route('/')
+# def hello():
+#     return 'Hello, World!'
+
+import serial
+from flask import Flask, render_template, request
+import sys
+import threading
+
+app = Flask(__name__)
+app.config.update(TEMPLATES_AUTO_RELOAD=True)
+ser = serial.Serial('/dev/ttyACM0', 57600, timeout=1)
+ser.reset_input_buffer()
+
+# @app.before_first_request
+# def before_first_request_func():
+#     while True:
+#         # ser.write(b"Hello from Raspberry Pi!\n")
+#         if ser.in_waiting>0:
+#             line = ser.readline().decode('latin-1').rstrip()
+#             print(line, file=sys.stderr)
+
+
+def run_app():
+    app.run(host='0.0.0.0',debug=False, threaded=True,use_reloader=False)
+
+def while_function():
+    while True:
+        # ser.write(b"Hello from Raspberry Pi!\n")
+        if ser.in_waiting>0:
+            line = ser.readline().decode('latin-1').rstrip()
+            print(line, file=sys.stderr)
+
+if __name__ == "__main__":
+    first_thread = threading.Thread(target=run_app)
+    second_thread = threading.Thread(target=while_function)
+    first_thread.start()
+    second_thread.start()
+    
+
+@app.route('/', methods=['POST', 'GET'])
+def hello():
+    # state = app.config['STATE']
+    # counter = state.get(counter)
+    # print("HERE", file=sys.stderr)
+    if request.method == 'POST':
+        var=request.form["test"]
+        print(var)
+        return "success"
+
+    return render_template('index.html')
+
+# @app.before_first_request
+# def before_first_request_func():
+#     print("This function will run once")
+
+
+
+
+# def webserver(state):
+#     app.config['STATE'] = state
+#     # If running on, say, a Raspberry Pi, use 0.0.0.0 so that
+#     # you can connect to the web server from your intranet.
+#     app.run(host='0.0.0.0', use_reloader=False, debug=True)
+
+# def main():
+#     state = SharedState()
+#     web_thread = threading.Thread(target=webserver, args=(state,))
+#     web_thread.start()
+
+#     state.set('counter', 0)
+#     while True:
+#         # Do whatever you want in the foreground thread
+#         state.set('counter', state.get('counter') + 1)
+#         print(state.get('counter'), file=sys.stderr)
+
+# class SharedState():
+#     def __init__(self):
+#         self.lock = threading.Lock()
+#         self.state = dict()
+
+#     def get(self, key):
+#         with self.lock:
+#             return self.state.get(key)
+
+#     def set(self, key, value):
+#         with self.lock:
+#             self.state[key] = value
